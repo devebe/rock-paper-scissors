b2eedc82f162cb3291e382b656f045cc4d0a9808
devebe <dvbirdja@gmail.com>
2022-08-06 19:59:01 +0200

    Rewrite ternary to more readable if-statement to keep score

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,20 +24,57 @@ function getplayerSelection() {
 function playRound() {
     let playerSelection = getplayerSelection();
     let computerSelection = getComputerChoice();
+
+    let outcome = ''
+    let pScore = 0;
+    let cScore = 0;
+
+    if (playerSelection === computerSelection) {
+        outcome = "It is a tie";
+    }
+    else if (
+        (playerSelection === 'Rock' && computerSelection === 'Paper')||
+        (playerSelection === 'Scissors' && computerSelection === 'Rock') ||
+        (playerSelection === 'Paper' && computerSelection === 'Scissors')
+        ) {
+            outcome = "The computer wins this round"
+            cScore = 1;
+    }
+    else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors')||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock')
+        ) {
+            outcome = "The player wins this round"
+            pScore = 1;
+    }
     
-    let outcome = '';
-    (playerSelection === computerSelection) ? outcome = "It is a tie!" :
-    (playerSelection === 'Rock' && computerSelection === 'Paper') ? outcome = "The computer wins!" :
-    (playerSelection === 'Rock' && computerSelection === 'Scissors') ? outcome = "You win!" :
-    (playerSelection === 'Scissors' && computerSelection === 'Rock') ? outcome = "The computer wins!" :
-    (playerSelection === 'Scissors' && computerSelection === 'Paper') ? outcome = "You win!" :
-    (playerSelection === 'Paper' && computerSelection === 'Scissors') ? outcome = "The computer wins!" :
-    (playerSelection === 'Paper' && computerSelection === 'Rock') ? outcome = "You win!" :
-    'This is weird'; // This exception case will never happen ??
+
+
     
-    outcome = `You chose ${playerSelection} and the computer played ${computerSelection}. ${outcome}`;
-    console.log(outcome);
+    // let outcome = '';
+    // (playerSelection === computerSelection) ? outcome = "It is a tie!" :
+    // (playerSelection === 'Rock' && computerSelection === 'Paper') ? outcome = "The computer wins!" :
+    // (playerSelection === 'Rock' && computerSelection === 'Scissors') ? outcome = "You win!" :
+    // (playerSelection === 'Scissors' && computerSelection === 'Rock') ? outcome = "The computer wins!" :
+    // (playerSelection === 'Scissors' && computerSelection === 'Paper') ? outcome = "You win!" :
+    // (playerSelection === 'Paper' && computerSelection === 'Scissors') ? outcome = "The computer wins!" :
+    // (playerSelection === 'Paper' && computerSelection === 'Rock') ? outcome = "You win!" :
+    // 'This is weird'; // This exception case will never happen ??
+    
+    outcome = `Player chose ${playerSelection} and the computer chose ${computerSelection}. ${outcome}`;
+    console.table(outcome,pScore,cScore);
     alert(outcome);
+    return pScore, cScore;
 }
 
-playRound();
+function game() {
+    const roundsOfGame = 5;
+    for (let i = 0; i < roundsOfGame; i++) {
+        let playerScore = 0;
+        let computerScore = 0;
+        playRound();
+    }
+}
+
+game();
