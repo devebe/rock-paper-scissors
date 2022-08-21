bb724710ac1d6b39ce79005fed506a12fa87a7b0
devebe <dvbirdja@gmail.com>
2022-08-21 19:42:42 +0200

    Refactor rock-paper-scissors to make it work

--- JS/set-ui-mode.js
+++ JS/set-ui-mode.js
@@ -1,7 +1,7 @@
 function setUiMode() {
     let today = new Date();
     let timeNow = today.getHours();
-    // timeNow = 20;
+    // timeNow = 12;
 
     const selectedClass = document.querySelector('.container');
     if (timeNow > 6 && timeNow < 18) {
